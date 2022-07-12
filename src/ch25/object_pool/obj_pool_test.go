efd39c40afbc25904aa6be040e9dfe128ba189f3
gaochuang <ienjoyarmlinux@163.com>
2022-07-12 20:09:04 +0800

    Update

--- src/ch25/object_pool/obj_pool_test.go
+++ src/ch25/object_pool/obj_pool_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestObjPool(t *testing.T) {
 	pool := NewObjPool(10)
-	for i := 0; i < 11; i++ {
+	for i := 0; i < 12; i++ {
 		if v, err := pool.GetObj(time.Second * 1); err != nil {
 			t.Error(err)
 		} else {
