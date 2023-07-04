80670eedf5356a83584fc6945d87a30e1f565a8c
gaochuang <ienjoyarmlinux@163.com>
2023-07-04 21:59:26 +0800

    Add slice and defer function demo

--- new_code/defer.go
+++ new_code/defer.go
@@ -9,9 +9,9 @@ func func1() {
 func func2() {
 	fmt.Println("func2()...")
 }
-
 func func3() {
 	fmt.Println("func3()...")
+
 }
 
 /*
@@ -38,10 +38,35 @@ func returnAndDefer() int {
 	return returnFunc()
 }
 
+/*
+recover错误拦截:
+1. Go语言提供拦截运行时pannic的内建函数recover。它可以是当前的程序从pannic的状态中恢复并重新获得流程控制权
+#### recover函数只有在defer函数中调用才有效#####
+*/
+
+func pannicAndRecover() {
+
+	//错误拦截要在产生错误前设置
+	defer func() {
+		//设置recover拦截错误信息
+		err := recover()
+		if err != nil {
+			//产生pannic异常，打印错误信息
+			fmt.Println("### will print pannic information ####")
+			fmt.Println(err)
+		}
+	}()
+
+	//主动产生一个pannic
+	panic("test pannic")
+}
+
 func main() {
 	defer func3()
 	defer func2()
 	defer func1()
 
 	fmt.Println(returnAndDefer())
+
+	pannicAndRecover()
 }
